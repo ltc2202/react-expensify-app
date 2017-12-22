0ad1b6fd3e29c07110df5c30ccd438031f544b40
Harry Le <ltcuong2202@gmail.com>
2017-12-23 01:18:15 +0700

    Remove sample expense data

--- src/app.js
+++ src/app.js
@@ -11,17 +11,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-
-
 const store = configureStore();
-
-store.dispatch(addExpense({description: 'Water bill', amount: 4500}));
-store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 120000}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jxs = (
     <Provider store={store}>

639942efeeed24d964fcd75ece4e13368c0aeb66
Harry Le <ltcuong2202@gmail.com>
2017-12-23 02:35:46 +0700

    Setup production webpack build

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -65,7 +65,7 @@ const mapStateToProps = (state) => ({
         filters: state.filters
 });  
 
-const mapDispatchToProps = (dipspatch) => ({
+const mapDispatchToProps = (dispatch) => ({
     setTextFilter: (text) => dispatch(setTextFilter(text)),
     sortByDate: () => dispatch(sortByDate()),
     sortByAmount: () => dispatch(sortByAmount()),
